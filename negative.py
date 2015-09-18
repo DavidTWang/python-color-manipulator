6fec04be0956222ef5d892d79ff72d7a68f67c10
godsgift <eltonsia@yahoo.com>
2015-09-17 18:05:12 -0700

    revised negative.py and added output image Negative.jpg

--- negative.py
+++ negative.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import PIL.ImageOps
 
+#error handling for when an image cannot be found
 def open_image(path):
 	image = None
 	try:
@@ -10,9 +11,11 @@ def open_image(path):
 		print(e)
 	return image
 
+#Find the image and turn it to 
+#negative image then save it with the name specified
 if __name__ == '__main__':
 	fileName = "images/ak.jpg"
 	image = open_image(fileName)
 	inverted_image = PIL.ImageOps.invert(image)
-	inverted_image.save("Negative.jpg")
+	inverted_image.save("images/Negative.jpg")
 	
