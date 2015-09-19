4dd9af1f532a6a3823f12904c301e419fe0dc373
Ben <benjamin3726@gmail.com>
2015-09-18 21:32:38 -0700

    clean up the code redeye.py

--- redeye.py
+++ redeye.py
@@ -2,7 +2,7 @@ from PIL import ImageSequence  # iterator object that can be used to loop over a
 from PIL import Image
 from PIL import ImageColor
 from PIL import ImageOps
-import os
+import os, sys
 
 def open_image(path):
 	image = None
@@ -18,45 +18,23 @@ def reduceRedPixel(pixel):
 	value=getRed(pixel)
   	# setRed(pixel,value*0.5)
 
-if __name__ == '__main__':
+def main(argv):
+
 	
 	# get image name for a user
-	#pic = raw_input("Which file you want to remove red eye? (include extension):")
+	# pic = raw_input("Which file you want to remove red eye? (include extension):")
 
-	#fileName = "images/" + pic
-	fileName = "images/re5.jpg"
+	if(len(argv) != 1):
+		print("Invalid number of arguments. Requires image path/filename.")
+		sys.exit(2)
+	else:
+		fileName = argv[0]
+
+
+	# fileName = "images/" + pic
+	#fileName = "images/re5.jpg"
 	image = open_image(fileName)
-	# os.system('xdg-open ' + fileName)
 
-
-	# image = image.convert('RGB')
-	# get image size of x and y
-	# (width, height) = image.size
-	
-	# max = 0
-
-	# for w in range(1, width):
-	# 	for h in range(1, height):
-	# 		r, g, b = image.getpixel((w,h))
-	# 		if r > max:
-	# 			max = r
-	# 		if (r in range (130, 255) and (g in range (1, 70) or b in range (1, 70))):
-	# 			r = 0
-	# 			g = 0
-	# 			b = 0
-	# 		#print r,g,b
-	# 		#r = int(r*0.5)
-	# 		image.putpixel((w,h), (r,g,b))
-	# 		print r,g,b
-	
-	# print max
-
-	# r, g, b = image.getpixel((187, 45))
-	# print r, g, b
-
-	#ImageOps.invert(image)
-	# save_image(image, 'red.jpeg')
-	#os.system('xdg-open ./red.jpeg')
 
 	# Get the color data of every pixel in the image
 	pixels = list(image.getdata())
@@ -75,3 +53,8 @@ if __name__ == '__main__':
 	# Write the pixel data back to images
 	image.putdata(pixels)
 	image.show()
+
+
+if __name__ == '__main__':
+	main(sys.argv[1:])
+
